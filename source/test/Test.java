89afa87ee9e4e7c89d3b7eade8f6f279d1913748
OscarJohansson1 <osjoh99@gmail.com>
2019-11-08 11:43:55 +0100

    Added tests for increment speed (Saab95 and Volvo240)

--- source/test/Test.java
+++ source/test/Test.java
@@ -22,4 +22,20 @@ public class Test {
     //public void testIncrementSpeedSaab95() {
      //   double prelSpeed = saab95.setCurrentSpeed();
     //}
+
+    @org.junit.Test
+    public void testIncrementSpeedSaab95() {
+        double saabSpeed = saab95.getCurrentSpeed();
+        saab95.incrementSpeed(1);
+
+        assert Double.compare(saab95.getCurrentSpeed(), saabSpeed) > 0 : "IncrementSpeed didn't increment speed";
+    }
+
+    @org.junit.Test
+    public void testIncrementSpeedVolvo240() {
+        double volvoSpeed = volvo240.getCurrentSpeed();
+        volvo240.incrementSpeed(1);
+
+        assert Double.compare(volvo240.getCurrentSpeed(), volvoSpeed) > 0 : "IncrementSpeed didn't increment speed";
+    }
 }
