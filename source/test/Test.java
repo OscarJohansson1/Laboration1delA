5600c0e7f9cb5e1515329514fd4b848805ab2d8b
David <davidlaessker@gmail.com>
2019-11-08 11:44:48 +0100

    test

--- source/test/Test.java
+++ source/test/Test.java
@@ -2,11 +2,9 @@ public class Test {
     Saab95 saab95 = new Saab95();
     Volvo240 volvo240 = new Volvo240();
 
-
     @org.junit.Test
     public void testSpeedFactorIfTurboOffSaab95() {
         saab95.setTurboOff();
-
         assert saab95.speedFactor() == saab95.getEnginePower() * 0.01 : "speedFactor with turbo off wrongly returns "
                 + saab95.speedFactor();
     }
@@ -14,12 +12,14 @@ public class Test {
     @org.junit.Test
     public void testSpeedFactorIfTurboOnSaab95() {
         saab95.setTurboOn();
-
         assert saab95.speedFactor() == saab95.getEnginePower() * 0.01 * 1.3: "speedFactor with turbo on wrongly " +
                 "returns " + saab95.speedFactor();
     }
 
-    //public void testIncrementSpeedSaab95() {
-     //   double prelSpeed = saab95.setCurrentSpeed();
-    //}
+    @org.junit.Test
+    public void testDecrementSpeedVolvo240(){
+        double speed = saab95.getCurrentSpeed();
+        saab95.decrementSpeed(1);
+        assert saab95.getCurrentSpeed() == saab95.getCurrentSpeed() - saab95.speedFactor() * 1;
+    }
 }
