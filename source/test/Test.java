65f626c5dae3af7ab0fed9681337af65ed1cd1f8
OscarJohansson1 <osjoh99@gmail.com>
2019-11-08 10:44:27 +0100

    Started adding tests

--- source/test/Test.java
+++ source/test/Test.java
@@ -1,3 +1,25 @@
 public class Test {
+    Saab95 saab95 = new Saab95();
+    Volvo240 volvo240 = new Volvo240();
 
+
+    @org.junit.Test
+    public void testSpeedFactorIfTurboOffSaab95() {
+        saab95.setTurboOff();
+
+        assert saab95.speedFactor() == saab95.getEnginePower() * 0.01 : "speedFactor with turbo off wrongly returns "
+                + saab95.speedFactor();
+    }
+
+    @org.junit.Test
+    public void testSpeedFactorIfTurboOnSaab95() {
+        saab95.setTurboOn();
+
+        assert saab95.speedFactor() == saab95.getEnginePower() * 0.01 * 1.3: "speedFactor with turbo on wrongly " +
+                "returns " + saab95.speedFactor();
+    }
+
+    //public void testIncrementSpeedSaab95() {
+     //   double prelSpeed = saab95.setCurrentSpeed();
+    //}
 }
