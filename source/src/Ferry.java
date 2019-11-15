66649f80c9fad84b536fed0a0e9ae6a20a181639
David <davidlaessker@gmail.com>
2019-11-15 11:36:13 +0100

    Finished ferry (?)

--- source/src/Ferry.java
+++ source/src/Ferry.java
@@ -1,12 +1,21 @@
 public class Ferry implements IMovable{
 
-    Transporter storage;
-    LoadingPlatform loadingPlatform;
-    Car car;
+    private Transporter storage;
+    private LoadingPlatform loadingPlatform;
 
     public Ferry(int maxStorage, int loadingAngle){
         this.storage = new Transporter(Car.class, maxStorage);
         this.loadingPlatform = new LoadingPlatform(Boolean.FALSE, loadingAngle);
+    }
+
+    public void enterCar(Car car){
+        if(loadingPlatform.isPlatformUp()){
+            storage.store(car);
+        }
+    }
+
+    public Car exitCar(){
+        return (Car) storage.removeFirstStored();
     }
 
     @Override
